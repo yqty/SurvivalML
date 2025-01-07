68d732686ef0b1d9ac7f6bab20519827f92f86f2
LIU Zaoqu <68080738+Zaoqu-Liu@users.noreply.github.com>
2025-01-07 15:47:19 +0800

    Add files via upload

---
+++ www/static/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?29fc7a4d6cfc9861bb08580fa48ba832";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
